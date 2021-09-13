9359a7d4fbbb1ed98f3b1e65b52a9191884e3de8
unknown <arokai1233@mail.ru>
2021-09-13 16:10:46 +0300

    Добавил программу перевод целсия в фаренгейты

---
+++ src/degrees/Degrees.java
@@ -0,0 +1,22 @@
+/*
+Дано значение температуры T в градусах Цельсия. 
+Определить значение этои же температуры по 
+Фаренгейту.
+Aleksandr Egorov JPTV20
+*/
+package degrees;
+import java.util.Scanner;
+
+
+public class Degrees {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите температуру по цельсию: ");
+        float tc = scanner.nextFloat(); // ввод пользователя
+        float tf = (tc * 9 / 5) + 32; // формула фаренгейта
+        System.out.print("Цельсии преобразованы в Фаренгейт: ");
+        System.out.println(tf);
+    }
+    
+}
