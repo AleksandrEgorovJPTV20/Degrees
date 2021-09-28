0813592018f96b4af118af07f8303a90378f01d0
unknown <arokai1233@mail.ru>
2021-09-28 18:28:40 +0300

    Добавил комментарии

--- src/degrees/Degrees.java
+++ src/degrees/Degrees.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Degrees {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); //Класс который позволяет считывать данные из разных источников. И из консоли тоже.
         System.out.print("Введите температуру по цельсию: ");
         float tc = scanner.nextFloat(); // ввод пользователя
         float tf = (tc * 9 / 5) + 32; // формула фаренгейта
